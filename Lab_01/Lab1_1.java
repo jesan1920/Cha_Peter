7e8a6b464d1a13ff48525bbd165aef451286ba12
pcha99 <pcha99@gmail.com>
2016-10-04 08:58:34 -0700

    New lab

---
+++ Lab_01/Lab1_1.java
@@ -0,0 +1,14 @@
+public class Lab1_1
+{
+	public static void main (String[]args)
+	{
+		System.out.println("/)-_-(\\");
+		System.out.println(" (o o) ");
+		System.out.println("  \\o/\\__-----.");
+		System.out.println("   \\        __    \\");
+		System.out.println("    \\|  /_/    \\  /\\__/");
+		System.out.println("     ||           \\\\");
+		System.out.println("     ||           // ");
+		System.out.println("     /|          /| ");
+	}
+}
