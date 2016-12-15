db5aec337afc6d4ba38e5efbe4677e55715cc07c
pcha99 <ChaY5074@sduhsd.lan>
2016-12-15 08:55:40 -0800

    lab 10 i caught up gg

---
+++ APLesson_10/expressionSolver.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class expressionSolver
+{
+	public static void main (String[]args)
+	{
+		Scanner kb = new Scanner (System.in);
+		System.out.println("Please input an equation:");
+		String expression = kb.nextLine();
+		ArrayList<String> equation = new ArrayList<>(Arrays.asList(expression.split(" ")));
+		
+		System.out.println(doEquation(equation).get(0));
+	}
+	
+	public static ArrayList doEquation(ArrayList<String> equation)
+	{
+		int i = 0;
+		while (i < equation.size())
+		{
+			if (equation.get(i).equals("*") || equation.get(i).equals("/"))
+			{
+				if (equation.get(i).equals("*"))
+					equation.set(i,"" + (Integer.parseInt(equation.get(i-1)) * Integer.parseInt(equation.get(i+1))));
+				else
+					equation.set(i,"" + (Integer.parseInt(equation.get(i-1)) / Integer.parseInt(equation.get(i+1))));
+				equation.remove(i-1);
+				equation.remove(i);
+			}
+			else
+				i++;
+		}
+		
+		i = 0;
+		while (i < equation.size())
+		{
+			if (equation.get(i).equals("+") || equation.get(i).equals("-"))
+			{
+				if (equation.get(i).equals("+"))
+					equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) + Integer.parseInt(equation.get(i+1))));
+				else
+					equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) - Integer.parseInt(equation.get(i+1))));
+				equation.remove(i-1);
+				equation.remove(i);
+			}
+			else
+				i++;
+		}
+		
+		return equation;
+	}
+}
