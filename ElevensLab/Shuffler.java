efad72d26abfa7475ec2cf3a9e843bed8042f3bd
pcha99 <pcha99@gmail.com>
2017-06-08 08:13:15 -0700

    elevens lab fix and final submission 2

--- ElevensLab/Shuffler.java
+++ ElevensLab/Shuffler.java
@@ -52,15 +52,15 @@ public class Shuffler {
 	 */
 	public static void perfectShuffle(int[] values) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
-		int[] shuffled = new int[51];
+		int[] shuffled = new int[values.length];
 		int k = 0;
-		for(int j = 0; j = 25; j++)
+		for(int j = 0; j < values.length/2; j++)
 		{
 			shuffled[k] = values[j];
 			k += 2;
 		}
 		k = 1;
-		for(int j = 26; j = 51; j++)
+		for(int j = values.length/2; j < values.length; j++)
 		{
 			shuffled[k] = values[j];
 			k += 2;
